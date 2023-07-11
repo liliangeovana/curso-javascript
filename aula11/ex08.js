41e90599c444417926ffa230a0adbdd0a96566cf
liliangeovana <119058582+liliangeovana@users.noreply.github.com>
2023-07-11 19:30:15 -0400

    exercicios js

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+var vlimite = 50
+var vel = 40
+
+if (vel > 50){//Condição simples
+    console.log(`Você ultrapassou a valocidade permitida. Multado!`)
+}
+console.log(`Sua velocidade é ${vel}KM/h. Dirija com cuidado`)
