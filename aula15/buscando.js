995e0b70bf2af4bc402449f2604ce7f53d7f8eda
liliangeovana <119058582+liliangeovana@users.noreply.github.com>
2023-07-18 16:52:35 -0400

    Update buscando.js

--- aula15/buscando.js
+++ aula15/buscando.js
@@ -1,13 +1,13 @@
 var num = [5, 3, 9, 8, 2]
 
-num.push(1)
-num.sort()
+num.push(1) // acresta o valor 1 na ultima posiçãp
+num.sort() // coloca em ordem
 
-console.log(num)
-console.log (`o vetor tem ${num.length} posições`)
-console.log (`O primeiro valor do vetor é ${num[0]}`)
+console.log(num) // exibe: [ 1, 2, 3, 5, 8, 9 ]
+console.log (`o vetor tem ${num.length} posições`) //exibe quantas posições
+console.log (`O primeiro valor do vetor é ${num[0]}`) //exibe qual o primeiro valor
 
-var pos = num.indexOf(2)
+var pos = num.indexOf(2) //busca se existe o valor 2 no array
 
 if (pos == -1){
     console.log ('Valor não encontrado')
